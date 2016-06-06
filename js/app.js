8c7f667acf46652eeb0574b163a21c6659d1490a
Tim Wright <mithgirw@gmail.com>
2016-06-06 10:31:00 -0600

    it appends the text input now

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-$(document).ready (function() {
-	$(".addbutton").click (function() {
-		alert ("you clicked it");
-		$("#newiteminput").val(function() {
-			$(".itemwords").append("this");
+$(document).ready(function() {
+	$("#addbutton").click(function() {
+		$("input").val(function() {
+			$("ul").append("<li>crap</li>");
+			alert($(this).val());
 		});
+		
 	});
+		
 	
 });
