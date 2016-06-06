33aeb7077a9b4d764a207b82aed780fb4d0eeac2
Tim Wright <mithgirw@gmail.com>
2016-06-06 11:56:00 -0600

    working on it

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
 	$("#addbutton").click(function() {
 		$("input").val(function() {
-			$(".buylist").append("<li>crap</li>");
+			$(".buylist").append("<li>Blah</li>");
 			
 		});
 		
+	});
+	$(".fa").click(function() {
+		$(this).parent().remove();
 	});
 		
 	
