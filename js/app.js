ff5d5fb9b30df2e77ce00dd977ce1204654be7b7
Tim Wright <mithgirw@gmail.com>
2016-06-07 09:56:16 -0600

    deleting list items is working

--- js/app.js
+++ js/app.js
@@ -2,12 +2,9 @@ $(document).ready(function() {
 	$("#addbutton").click(function() {
 			$('.buylist').append('<li>' + '<i class="fa fa-check-circle-o fa-2x"></i>' + $('#iteminput').val() + '<i class="fa fa-trash fa-2x"></i>' + '</li>');
 			$('input[type=text], textarea').val('');
-			
-
-		
 	});
-	$('.fa-trash').on('click', function(event) {
-		$('.fa-trash').parent().remove();
+	$('ul').on('click', '.fa-trash', function(event) {
+		$(this).parent().remove();
 	});
 		
 	
